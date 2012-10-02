2067ef567c506d35d133dae601e27a72d8822086
OpenSourceAnonymous <chris.lee@qrhook.com>
2012-10-02 16:32:34 -0400

    Organized all the files well

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+	Author: OpenSourceAnonymous
+	Desc: 	This is the main js file for the web application
+*/
+
+
+var Main = {
+	init: function(){
+
+	},
+	resize:function(){
+		Win.init();
+	}
+}
